75868dea511ce7851641b8393e3ae4d61326e17e
SangHyukIM <301kd@naver.com>
2021-08-17 14:29:05 +0900

    State Updata

--- src/App.js
+++ src/App.js
@@ -1,54 +1,28 @@
  import PropTypes from "prop-types"; 
+import { Component } from "react";
  // 터미널 npm i porp-types 설치 후
  // propType 호출 
 
-const foodILike = [{
-   id:1,
-    name: "Kimchi",
-    image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
-    rating:4
-    },
-    {
-     id:2,  
-    name: "bulgogi",
-    image: "https://www.google.com/search?q=%EB%B6%88%EA%B3%A0%EA%B8%B0&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk02ziUIXmsNvR7TuYKh1cpJGpdpH8g:1593509047151&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi-p87lm6nqAhVbUd4KHVSWBBUQ_AUoAXoECBgQAw&biw=1440&bih=701&dpr=2#imgrc=kjZoAmvHGo-KfM",
-    rating:4.9
-    },
-    {
-     id:3,  
-    name: "kimbap",
-    image: "https://www.google.com/search?q=%EA%B9%80%EB%B0%A5&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk03dn_nrK6M5JlKKfD5krm8w3qTmGQ:1593509069629&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjqn6rwm6nqAhXDQN4KHYLUA4EQ_AUoAXoECBgQAw&biw=1440&bih=701#imgrc=Xz3xQoYZw3bqEM",
-    rating:4.8
-    },
-    {
-     id:4,  
-    name: "samgyetang",
-    image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
-    rating:4.7
-    }] 
- 
 
-function Food({name, picture, rating}){ 
-  return <div>
-    <h3>I like {name}</h3> 
-    <h4>{rating}/5</h4>
-    <img src={picture} alt={name}/>
-  </div> 
-}
-
-Food.propTypes = {  // propType 검증 (불일치시 console err)
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-}
-function App() {
-  return (
-    <div className="App">
-      {foodILike.map(dish => (
-      <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
-      ))}
+class App extends Component{
+  state = {
+    count: 0
+  }
+  add = () =>{
+    this.setState(current => ({count: current.count + 1 }));
+  };
+  minus = () =>{
+    this.setState(current => ({count: current.count - 1 }));
+  };
+  render(){
+    return <div>
+      <h1>
+      The number is: {this.state.count}
+      </h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
     </div>
-  );
+  }
 }
 
 export default App;
